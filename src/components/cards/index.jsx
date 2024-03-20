714f11e9e76927bb12ca7871ae05b1b59770b15a
RafalFDS2 <rafael.ferraz@picpay.com>
2024-03-20 07:05:15 -0300

    little update

--- src/components/cards/index.jsx
+++ src/components/cards/index.jsx
@@ -1,9 +1,9 @@
 import "./Card.css";
 
-const Cards = ({ object }) => {
+const Cards = ({ object, pokeName }) => {
   return (
     <>
-      <h1>Card</h1>
+      <h1 className="pokemon-name-card"> Card</h1>
       {object.map((pokemon) => (
         <div key={pokemon.id}>
           <img src={pokemon.images.small} alt={pokemon.name} />
