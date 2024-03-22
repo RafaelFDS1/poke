112dd8d67e66821dd13dcb671474cb6bb54b061d
RafaelFDS1 <rafael_fds@outlook.com>
2024-03-22 07:14:56 -0300

    update

--- src/utils/pokemon.js
+++ src/utils/pokemon.js
@@ -1,21 +1,29 @@
 const pokeRequest = (pokemon = "") => {
   let url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
-  return fetch(url).then((response) => {
-    if (!response.ok) {
-      throw new Error(response.statusText);
-    }
-    return response.json();
-  });
+  return fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      return response.json();
+    })
+    .catch((err) => {
+      console.error(err);
+    });
 };
 
 const tcgRequest = (pokemon = "") => {
   let url = `https://api.pokemontcg.io/v2/cards?q=!name:${pokemon}&pageSize=10`;
-  return fetch(url).then((response) => {
-    if (!response.ok) {
-      throw new Error(response.statusText);
-    }
-    return response.json();
-  });
+  return fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      return response.json();
+    })
+    .catch((err) => {
+      console.error(err);
+    });
 };
 
 export { pokeRequest, tcgRequest };
